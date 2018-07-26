80901ee26cd40bebbcd224054fd2349aea6e2d6c
JoeNoPhoto <yourboybstar@gmail.com>
2018-07-26 13:20:44 -0700

    Updated status for enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_024152) do
-
+ActiveRecord::Schema.define(version: 2018_07_26_194112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_24_024152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
