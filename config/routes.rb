cad2b4a34dabfd559e30e02d368aee09320d9292
JoeNoPhoto <yourboybstar@gmail.com>
2018-07-26 14:49:16 -0700

    updated routes on master

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,13 @@ Rails.application.routes.draw do
   get 'pages/contact'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#about'
+
+  resources :blogs
+
+  root to: 'pages#home'
 end
