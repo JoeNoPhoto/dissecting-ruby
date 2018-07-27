35928b1d3839df4380f53132a382d9694dd4a13b
JoeNoPhoto <yourboybstar@gmail.com>
2018-07-27 13:56:25 -0700

    Implemented image defaults for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_195731) do
-
+ActiveRecord::Schema.define(version: 2018_07_26_194112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,9 +21,7 @@ ActiveRecord::Schema.define(version: 2018_07_27_195731) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
-    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
-    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -56,11 +53,4 @@ ActiveRecord::Schema.define(version: 2018_07_27_195731) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "topics", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "blogs", "topics"
 end
