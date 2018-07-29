d830622c800cd0c1b45d18dca07260247fc4f516
JoeNoPhoto <yourboybstar@gmail.com>
2018-07-29 13:07:53 -0700

    Implemented form configuration for newsted attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,16 +16,12 @@ class PortfoliosController < ApplicationController
   # GET /portfolios/new
   def new
     @portfolio_item = Portfolio.new
-  end
-
-  # GET /blogs/1/edit
-  def edit
-    @portfolio_item = Portfolio.find(params[:id])
+    3.times { @portfolio_item.technologies.build }
   end
 
   # POST /portfolios
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio item has been added.' }
@@ -33,6 +29,11 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
+  end
+
+  # GET /blogs/1/edit
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   # PATCH/PUT /blogs/1
