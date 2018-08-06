94333d54e4e1d4d91ddc8a461d6b0d2ba657dbc0
JoeNoPhoto <yourboybstar@gmail.com>
2018-08-06 14:20:12 -0700

    Implemented initial design for portfolio index page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,7 +7,7 @@ class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.javascript
-    where(subtitle: "Javascript")
+    where(subtitle: 'Javascript')
   end
 
   scope :rubyrails, -> { where(subtitle: 'Ruby on Rails') }
