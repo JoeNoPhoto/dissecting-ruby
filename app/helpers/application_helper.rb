bb7a5eebdf98fbdbc9bbd6485840baf29c67ac16
JoeNoPhoto <yourboybstar@gmail.com>
2018-08-06 20:01:53 -0700

    Implemented refactor for blogs and statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,41 @@ module ApplicationHelper
   def copyright_generator
     JoeNoCoViewTool::Renderer.copyright 'JoeNo Co.', 'All Rights Reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      }
+    ]
+  end
+
+  def nav_helper(style, _tag_type=nil)
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "#{_tag_type}<a href='#{item[:url]}' class='#{style} #{active? item[:url]}'> #{item[:title]}</a>"
+    end
+    nav_links.html_safe
+  end
+
+  def active?(path)
+    'active' if current_page? path
+  end
 end
