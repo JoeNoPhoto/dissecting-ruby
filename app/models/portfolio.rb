4e24fa7ba4b2d38083a7fea37102d039c31a5044
JoeNoPhoto <yourboybstar@gmail.com>
2018-08-14 11:58:48 -0700

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,11 +2,14 @@ class Portfolio < ApplicationRecord
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
-  has_one_attached :main_image
-  has_one_attached :thumb_image
+  # has_one_attached :main_image
+  # has_one_attached :thumb_image
 
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
   def self.javascript
     where(subtitle: 'Javascript')
